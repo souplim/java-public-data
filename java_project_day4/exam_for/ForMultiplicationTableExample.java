5248d08e18e66c2c2f808fdb920a62d50aeeb184
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-14 17:38:51 +0900

    Update ForMultiplicationTableExample.java

--- java_project_day4/exam_for/ForMultiplicationTableExample.java
+++ java_project_day4/exam_for/ForMultiplicationTableExample.java
@@ -10,11 +10,22 @@ package exam_for;
  */
 public class ForMultiplicationTableExample {
     public static void main(String[] args) {
+        // [요구1]
         for(int i=2; i<10; i++){
             for(int j=1; j<10; j++){
-                System.out.prinf("%d * %d = %d",i,j,i*j);
+                System.out.printf("%d * %d = %d\n",i,j,i*j);
             }
             System.out.println();
         }
+
+        // [요구2]
+        for(int i=1; i<10; i++){
+            for(int j=2; j<10; j++){
+                System.out.printf("%d * %d = %2d\t",j,i,j*i);
+            }
+            System.out.println();
+        }
+
+
     }
 }
