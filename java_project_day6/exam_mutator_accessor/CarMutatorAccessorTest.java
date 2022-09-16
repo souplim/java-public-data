473a7b231c29e953da8e962c63dfc2ca402208f4
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-16 17:31:13 +0900

    Update CarMutatorAccessorTest.java

--- java_project_day6/exam_mutator_accessor/CarMutatorAccessorTest.java
+++ java_project_day6/exam_mutator_accessor/CarMutatorAccessorTest.java
@@ -5,42 +5,27 @@ class CarMutatorAccessor {
     private int gear; // 기어단수
     private String color; // 색상
 
+    // 생성자
     CarMutatorAccessor(){ }
-
-    CarMutatorAccessor(int s, int g, String c){
+    CarMutatorAccessor(int speed, int gear, String color){
         this.speed = speed;
         this.gear = gear;
         this.color = color;
     }
 
-    public void carData(int speed, int gear, String color){
-        this.speed = speed;
-        this.gear = gear;
-        this.color = color;
-    }
-
-    public void setSpeed(int speed){
+    // 설정자(Setter) / 접근자(Getter)
+    public void setSpeed(int speed){ // 속도가 0 미만일 때 거르는 조건문 추가해주어야
+        if(speed<0)
+            this.speed = 0;
         this.speed = speed;
     }
-    public int getSpeed(){
-        return speed;
-    }
+    public int getSpeed(){ return speed; }
+    public void setGear(int gear){ this.gear = gear; }
+    public int getGear(){ return gear;}
+    public void setColor(String color){ this.color = color; }
+    public String getColor(){ return color; }
 
-    public void setGear(int gear){
-        this.gear = gear;
-    }
-    public int getGear(){
-        return gear;
-    }
-
-    public void setColor(String color){
-        this.color = color;
-    }
-    public String getColor(){
-        return color;
-    }
-
-
+    // 메소드 정의
     public void speedUp(int s){ // 속도 증가 메서드. s: 매개변수
         speed+=s;
     }
@@ -52,48 +37,35 @@ class CarMutatorAccessor {
     }
 
     public String toString(){ // 객체의 상태를 문자열로 반환하는 메서드
-        return "속도: "+speed+"기어: "+gear+"색상: "+color;
+        return "자동차 상태정보 [ 속도: "+speed+", 기어: "+gear+", 색상: "+color+" ]";
     }
 }
 
 public class CarMutatorAccessorTest {
     public static void main(String[] args){
-        // 클래스명 참조변수 = new 클래스명();
-        CarMutatorAccessor c = new CarMutatorAccessor();// 첫번째 객체 생성
-
-        // 각각의 필드에 값을 설정해주는 방법: 참조변수명.필드 = 값;
-//        c.speed = 0; // 객체의 필드 변경. 접근제어자가 public 이어서 가능
-//        c.gear = 1; // 객체의 필드 변경
-//        c.color = "red"; // 객체의 필드 변경
-
+        // 첫번째 객체 생성
+        CarMutatorAccessor c = new CarMutatorAccessor();
+        // 설정자 메소드 호출
         c.setSpeed(0);
         c.setGear(1);
         c.setColor("red");
 
+        // 두번째 객체 생성
+        CarMutatorAccessor c2 = new CarMutatorAccessor(60, 3, "blue");
+
+        // 접근자 메소드 호출
         System.out.println(c.getSpeed());
         System.out.println(c.getGear());
         System.out.println(c.getColor());
+        System.out.println(c2.getSpeed());
+        System.out.println(c2.getGear());
+        System.out.println(c2.getColor());
 
         // 객체의 메서드 호출: 참조변수명.메서드()
         c.speedUp(20);
+        c2.speedDown(30);
+        // 출력
         c.printData(); // 출력방법 1
-        System.out.println("Car: "+c.toString()); // 출력방법2
-
-        // 두번째 객체 생성
-//        Car c2 = new Car();
-//        c2.speed = 60;
-//        c2.gear = 3;
-//        c2.color = "blue";
-
-        CarMutatorAccessor c2 = new CarMutatorAccessor();
-        c2.carData(60, 3, "blue");
-
-//        Car c2 = Car(60, 3, "blue");
-
-
-
-        c2.speedDown(40);
-        c2.printData();
-        System.out.println("Car2: "+c.toString());
+        System.out.println("Car: "+c2.toString()); // 출력방법2
     }
 }
