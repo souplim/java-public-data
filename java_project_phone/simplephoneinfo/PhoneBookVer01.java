f88ea1cd0d39ea3eeb4ca0ff2fc914d892d829a7
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-16 15:22:05 +0900

    Add files via upload

---
+++ java_project_phone/simplephoneinfo/PhoneBookVer01.java
@@ -0,0 +1,23 @@
+package simplephoneinfo;
+
+/* 필드는 private 선언
+ * 설정자(Setter) / 접근자(Getter)
+ * 생성자
+ * 출력은 toString()이 아니라 showPhoneInfo()로 설정
+ * [실행 예시]
+ * name: 임미경
+ * phone: 010-2345-8745
+ * birth: 1965.03.28.
+ */
+
+class phoneinfo {
+    private String name;
+    private String phoneNumber;
+    private String birthday;
+}
+
+public class PhoneBookVer01 {
+    public static void main(String[] args) {
+
+    }
+}
