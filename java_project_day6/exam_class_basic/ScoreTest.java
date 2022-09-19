2777e9ee27771182b8d19ae9b4ccda665d6189f0
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-19 11:54:17 +0900

    Update ScoreTest.java

--- java_project_day6/exam_class_basic/ScoreTest.java
+++ java_project_day6/exam_class_basic/ScoreTest.java
@@ -2,8 +2,10 @@ package exam_class_basic;
 
 import java.util.Scanner;
 
+import static exam_class_basic.MovieTest.scanner;
+
 // 성적을 구현하는 Score 클래스를 정의하자.
-/* [필드 구현] 이름/국어/영어/수학/총점을 저장하고자 한다.
+/* [필드 구현] 이름/국어/영어/수학/총점을 저장하고자 한다. public
  * [메서드 구현] 필드에 값을 설정할 수 있는 기능, 점수들의 합을 구하는 기능, 평균을 구하는 기능,
  *             학점을 구하는 기능, 모든 필드를 출력할 수 있는 기능을 포함하도록 한다.
  * [출력 결과] 사용자로 하여금 이름 및 점수들을 입력받아 아래의 결과를 출력하도록 한다.
@@ -23,18 +25,11 @@ import java.util.Scanner;
  * 홍길동 90   90   90   270   90.0  A
  */
 class Score { //이름/국어/영어/수학/총점
-    private String name;
-    private int kor;
-    private int eng;
-    private int math;
-
-//    Score(){ }
-//    Score(String name, int kor, int eng, int math){
-//        this.name = name;
-//        this.kor = kor;
-//        this.eng = eng;
-//        this.math = math;
-//    }
+    public String name;
+    public int kor;
+    public int eng;
+    public int math;
+    public int total;
 
     public void scoreData(String name, int kor, int eng, int math){
         this.name = name;
@@ -42,65 +37,72 @@ class Score { //이름/국어/영어/수학/총점
         this.eng = eng;
         this.math = math;
     }
-    public int sum(){
-        return kor+eng+math;
+
+    // 총점을 필드에 구현하라는 요구가 있었기 때문에 return값 반환할 필요 없이 필드값에 대입만 해주면 됨!
+    // 요구사항에 따라 필드값에 대입할 지, return값으로 반환할지를 결정
+    public void getTotal(){
+        total = kor+eng+math;
     }
     public double avg(){
-        return (kor+eng+math)/3.0;
+        return total/3.0;
     }
     public char grade(){
+        char hakjum = '\0'; // null을 의미 '0'줘버리면 48주는 것
         if(avg()>=90)
-            return 'A';
+            hakjum = 'A';
         else if(avg()>=80)
-            return 'B';
+            hakjum = 'B';
         else if(avg()>=70)
-            return 'C';
+            hakjum = 'C';
         else if(avg()>=60)
-            return 'D';
+            hakjum = 'D';
         else
-            return 'F';
+            hakjum = 'F';
+
+        return hakjum;
     }
 
     public String toString(){
-        return String.format("%s\t%d\t%d\t%d\t %d\t %.1f\t%c", name, kor, eng, math, sum(), avg(), grade());
+        return String.format("%s\t%d\t%d\t%d\t %d\t %.1f\t%c", name, kor, eng, math, total, avg(), grade());
     }
 }
 
 public class ScoreTest {
+    // 데이터를 입력받기 위한 아래의 inputData메서드 Q.Score 클래스안에 정의하지 않는 이유?
+    // 이는 실행할 때 값을 어떻게 받기 위한 것이지 Score클래스의 주요 속성은 아님. 굳이 Score 클래스에 만들어서 메모리 줄 필요 없음
+    public static void inputData(Score score){ // 매개변수 : 클래스명 참조변수 = 주소값
+        // 필드에 직접 대입 Score score = s1
+        System.out.print("이름 입력 : ");
+        score.name = scanner.nextLine();
+
+        System.out.print("국어 점수 입력 : ");
+        score.kor = scanner.nextInt();
+
+        System.out.print("영어 점수 입력 : ");
+        score.eng = scanner.nextInt();
+
+        System.out.print("수학 점수 입력 : ");
+        score.math = scanner.nextInt();
+        scanner.nextLine();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         Score s1 = new Score();
-        Score s2 = new Score();
-        Score s3 = new Score();
-
-        System.out.print("이름, 국어점수, 영어점수, 수학점수를 입력하시오 : ");
-        String name1 = scanner.next();
-        int kor1 = scanner.nextInt();
-        int eng1 = scanner.nextInt();
-        int math1 = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("이름, 국어점수, 영어점수, 수학점수를 입력하시오 : ");
-        String name2 = scanner.next();
-        int kor2 = scanner.nextInt();
-        int eng2 = scanner.nextInt();
-        int math2 = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("이름, 국어점수, 영어점수, 수학점수를 입력하시오 : ");
-        String name3 = scanner.next();
-        int kor3 = scanner.nextInt();
-        int eng3 = scanner.nextInt();
-        int math3 = scanner.nextInt();
-
-        s1.scoreData(name1, kor1, eng1, math1);
-        s2.scoreData(name2, kor2, eng2, math2);
-        s3.scoreData(name3, kor3, eng3, math3);
-
 //        s1.scoreData("홍길동", 90, 75, 61);
+        inputData(s1); // 매개변수로 넣지 않는 이상 메인메서드 밖에서 객체 사용할 수 없다. 사용자에게 입력값을 받기 때문에 생긴 일
+        s1.getTotal();
+
+        Score s2 = new Score();
 //        s2.scoreData("김철수", 55, 56, 46);
+        inputData(s2);
+        s2.getTotal();
+
+        Score s3 = new Score();
 //        s3.scoreData("이진희", 90, 90, 90);
+        inputData(s3);
+        s3.getTotal();
 
         System.out.println("===================================");
         System.out.println("이름  국어  영어  수학  총점  평균  학점");
