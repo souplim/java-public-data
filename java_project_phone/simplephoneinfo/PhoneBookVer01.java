d79663f55efe2707835881ea5684b77b8376eb5b
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-19 17:43:16 +0900

    Update PhoneBookVer01.java

--- java_project_phone/simplephoneinfo/PhoneBookVer01.java
+++ java_project_phone/simplephoneinfo/PhoneBookVer01.java
@@ -6,12 +6,14 @@ package simplephoneinfo;
  * 필드는 private 선언
  * 설정자(Setter) / 접근자(Getter)
  * 생성자
- * 출력은 toString()이 아니라 showPhoneInfo()로 설정 Q.WHY?
+ * 출력은 toString()이 아니라 showPhoneInfo()로 설정 Q.WHY? A. 반환값 없이 바로 출력하는 메서드
  * [실행 예시]
  * name: 임미경
  * phone: 010-2345-8745
  * birth: 1965.03.28.
  */
+
+import java.util.Scanner;
 
 class PhoneInfo {
     private String name;
@@ -55,26 +57,50 @@ class PhoneInfo {
 }
 
 public class PhoneBookVer01 {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void inputPhoneinfo(PhoneInfo pi){
+        scanner.nextLine();
+        System.out.print("이름 : ");
+        pi.setName(scanner.nextLine());
+        System.out.print("전화번호 : ");
+        pi.setPhoneNumber(scanner.nextLine());
+        System.out.print("생년월일 : ");
+        pi.setBirthday(scanner.nextLine());
+    }
+
     public static void main(String[] args) {
         // (1) setter / getter 사용
         PhoneInfo pi = new PhoneInfo();
         pi.setName("임미경");
         pi.setPhoneNumber("010-2345-6573");
         pi.setBirthday("1965.03.28.");
+        pi.showPhoneInfo();
 
         PhoneInfo pi2 = new PhoneInfo();
         pi2.setName("김현수");
         pi2.setPhoneNumber("011-6435-1249");
+        pi2.showPhoneInfo();
 
         // (2) 생성자 사용
         PhoneInfo pi3 = new PhoneInfo("임미경", "010-2345-6573", "1965.03.28.");
-
-        // (3)
         PhoneInfo pi4 = new PhoneInfo("김현수", "011-6435-1249");
-
-        pi.showPhoneInfo();
-        pi2.showPhoneInfo();
         pi3.showPhoneInfo();
         pi4.showPhoneInfo(); // birthday 안 적은 사람 birthday 조차 안 뜨게 하려면?
+
+        // (3) 데이터 입력받기
+        int i = 0;
+        int choice = 1; // 데이터 입력과 종료 변수
+        do{
+            System.out.printf("선택하세요\n1. 데이터 입력\n2. 프로그램 종료\n선택 : ");
+            choice = scanner.nextInt();
+            if(choice==2){ break; }
+            else if(choice==1){
+                PhoneInfo pi5 = new PhoneInfo();
+                inputPhoneinfo(pi5);
+                pi5.showPhoneInfo();
+            }
+        } while(!(choice==2));
+        System.out.println("프로그램을 종료합니다.");
+        scanner.close();
     }
 }
