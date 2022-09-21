7c261fbdf0eb95ffef3e61f29fe0dac57a0913bd
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-21 14:58:02 +0900

    Update and rename java_project_day7/exam_inheritance/EmployeeTest.java to java_project_day7/exam_class_extends/EmployeeTest.java

--- java_project_day7/exam_class_extends/EmployeeTest.java
+++ java_project_day7/exam_class_extends/EmployeeTest.java
@@ -1,4 +1,4 @@
-package exam_inheritance;
+package exam_class_extends;
 
 class Employee2 {
     private String name;
@@ -6,7 +6,7 @@ class Employee2 {
     private String phoneNumber;
     private int salary;
 
-    public void set(String name, String address, String phoneNumber, int salary){
+    public void setEmpData(String name, String address, String phoneNumber, int salary){
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
@@ -22,7 +22,8 @@ class Manager extends Employee2{
     private int bonus;
     private String job;
 
-    public void setBonusJob(int bonus, String job){
+    public void setMgrData(String name, String address, String phoneNumber, int salary, int bonus, String job){
+        super.setEmpData(name, address, phoneNumber, salary);
         this.bonus = bonus;
         this.job = job;
     }
@@ -35,11 +36,10 @@ class Manager extends Employee2{
 public class EmployeeTest{
     public static void main(String[] args) {
         Employee2 e = new Employee2();
-        e.set("홍길동", "역삼동", "010-1234-5678", 500);
+        e.setEmpData("홍길동", "역삼동", "010-1234-5678", 500);
 
         Manager m = new Manager();
-        m.set("임은재", "역삼동","010-1234-5678", 700);
-        m.setBonusJob(5000, "웹개발자");
+        m.setMgrData("임은재", "역삼동","010-1234-5678", 700, 5000, "웹개발자");
 
         System.out.println(e);
         System.out.println(m);
