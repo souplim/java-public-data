af981cf4a6f716eb613862149256c54243dd58b2
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-23 14:53:26 +0900

    Update MessageSenderTest.java
    생성자 추가, 매개변수 통한 값 설정

--- java_project_day7/exam_abstract_class/MessageSenderTest.java
+++ java_project_day7/exam_abstract_class/MessageSenderTest.java
@@ -4,15 +4,24 @@ abstract class MessageSender{
     String title;
     String senderName;
 
-    public abstract void sendMessage();
+    public abstract void sendMessage(String parameter);
 }
 
 class EMailSender extends MessageSender{
     String senderAddr;
     String emailBody;
 
-    public void sendMessage(){
-        System.out.println("제목: "+title+"\n보내는 사람: "+senderName+" "+senderAddr+"\n받는 사람: javaone@naver.com\n내용: "+emailBody);
+    public EMailSender(){ }
+    public EMailSender(String title, String senderName, String senderAddr, String emailBody){
+        this.title = title;
+        this.senderName = senderName;
+        this.senderAddr = senderAddr;
+        this.emailBody = emailBody;
+    }
+
+    @Override
+    public void sendMessage(String receiverAddr){
+        System.out.println("제목: "+title+"\n보내는 사람: "+senderName+" "+senderAddr+"\n받는 사람: "+receiverAddr+"\n내용: "+emailBody);
     }
 }
 
@@ -20,29 +29,27 @@ class SMSSender extends MessageSender {
     String returnPhoneNo;
     String message;
 
-    public void sendMessage(){
-        System.out.println("제목: "+title+"\n보내는 사람: "+senderName+"\n전화번호: 010-000-0000\n회신 전화번호: "+returnPhoneNo+"\n메시지 내용: "+message);
+    public SMSSender(){ }
+    public SMSSender(String title, String senderName, String returnPhoneNo, String message){
+        this.title = title;
+        this.senderName = senderName;
+        this.returnPhoneNo = returnPhoneNo;
+        this.message = message;
+    }
+
+    @Override
+    public void sendMessage(String phoneNo){
+        System.out.println("제목: "+title+"\n보내는 사람: "+senderName+"\n전화번호: "+phoneNo+"\n회신 전화번호: "+returnPhoneNo+"\n메시지 내용: "+message);
     }
 }
 public class MessageSenderTest {
     public static void main(String[] args) {
-        EMailSender e = new EMailSender();
-        SMSSender s = new SMSSender();
-
-        e.title = "생일을 축하합니다.";
-        e.senderName = "고객센터";
-        e.senderAddr = "admin@dukeeshop.co.kr";
-        e.emailBody = "10% 할인쿠폰이 발행되었습니다.";
-
-        s.title = "생일을 축하합니다.";
-        s.senderName = "고객센터";
-        s.returnPhoneNo = "02-000-0000";
-        s.message = "10% 할인쿠폰이 발행되었습니다.";
+        EMailSender e = new EMailSender("생일을 축하합니다.", "고객센터", "admin@dukeeshop.co.kr", "10% 할인쿠폰이 발행되었습니다.");
+        SMSSender s = new SMSSender("생일을 축하합니다.", "고객센터", "02-000-0000", "10% 할인쿠폰이 발행되었습니다.");
 
         System.out.println("-------------------------------");
-        e.sendMessage();
+        e.sendMessage("javaone@naver.com");
         System.out.println("-------------------------------");
-        s.sendMessage();
-
+        s.sendMessage("010-000-0000");
     }
 }
