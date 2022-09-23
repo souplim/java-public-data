fc3f1185c3bf15931273eaab062ae7836b7c33ef
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-23 15:17:40 +0900

    Update MessageSenderTest.java
    getter/setter 추가

--- java_project_day7/exam_abstract_class/MessageSenderTest.java
+++ java_project_day7/exam_abstract_class/MessageSenderTest.java
@@ -1,45 +1,61 @@
 package exam_abstract_class;
 
 abstract class MessageSender{
-    String title;
-    String senderName;
+    private String title;
+    private String senderName;
 
-    public abstract void sendMessage(String parameter);
+    MessageSender(String title, String senderName){
+        this.title = title;
+        this.senderName = senderName;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getSenderName() { return senderName; }
+    public void setSenderName(String senderName) { this.senderName = senderName; }
+
+    public abstract void sendMessage(String recipient);
 }
 
 class EMailSender extends MessageSender{
-    String senderAddr;
-    String emailBody;
+    private String senderAddr;
+    private String emailBody;
 
-    public EMailSender(){ }
+    public String getSenderAddr() { return senderAddr; }
+    public void setSenderAddr(String senderAddr) { this.senderAddr = senderAddr; }
+    public String getEmailBody() { return emailBody; }
+    public void setEmailBody(String emailBody) { this.emailBody = emailBody; }
+
     public EMailSender(String title, String senderName, String senderAddr, String emailBody){
-        this.title = title;
-        this.senderName = senderName;
+        super(title, senderName); // 부모 클래스의 생성자 호출
         this.senderAddr = senderAddr;
         this.emailBody = emailBody;
     }
 
     @Override
-    public void sendMessage(String receiverAddr){
-        System.out.println("제목: "+title+"\n보내는 사람: "+senderName+" "+senderAddr+"\n받는 사람: "+receiverAddr+"\n내용: "+emailBody);
+    public void sendMessage(String recipient){
+        System.out.println("제목: "+getTitle()+"\n보내는 사람: "+getSenderName()+getSenderAddr()+"\n받는 사람: "+recipient+"\n내용: "+getEmailBody());
     }
 }
 
 class SMSSender extends MessageSender {
-    String returnPhoneNo;
-    String message;
+    private String returnPhoneNo;
+    private String message;
 
-    public SMSSender(){ }
     public SMSSender(String title, String senderName, String returnPhoneNo, String message){
-        this.title = title;
-        this.senderName = senderName;
+        super(title, senderName);
         this.returnPhoneNo = returnPhoneNo;
         this.message = message;
     }
 
+    public String getReturnPhoneNo() { return returnPhoneNo; }
+    public void setReturnPhoneNo(String returnPhoneNo) { this.returnPhoneNo = returnPhoneNo; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
     @Override
-    public void sendMessage(String phoneNo){
-        System.out.println("제목: "+title+"\n보내는 사람: "+senderName+"\n전화번호: "+phoneNo+"\n회신 전화번호: "+returnPhoneNo+"\n메시지 내용: "+message);
+    public void sendMessage(String recipient){
+        System.out.println("제목: "+getTitle()+"\n보내는 사람: "+getSenderName()+"\n전화번호: "+recipient+"\n회신 전화번호: "+getReturnPhoneNo()+"\n메시지 내용: "+getMessage());
     }
 }
 public class MessageSenderTest {
