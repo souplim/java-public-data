f53cc6a66c6436adb143b9f1c7f9f16a24bbc5fc
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-27 15:16:24 +0900

    Add files via upload

---
+++ java_project_day7/exam_interface/BoxTest.java
@@ -0,0 +1,49 @@
+package exam_interface;
+
+/* 자바에서는 객체의 크기를 비교하는데 사용되는 Comparable 인터페이스를 제공해 준다.
+ * public interface Comparable {
+       // 이 객체가 다른 객체보다 크면 1, 같으면 0, 작으면 -1을 반환한다.
+       int compareTo(Object other);
+ * }
+*/
+
+@SuppressWarnings("rawtypes")
+class Box implements Comparable{
+    private double volume;
+
+    public Box(double volume){
+        this.volume = volume;
+    }
+
+    public double getVolume(){ return volume; }
+
+    @Override
+    public int compareTo(Object obj){ // Object obj = b2 업캐스팅
+        Box other = (Box)obj; // (서브클래스명)부모클래스의 참조변수. 다운캐스팅
+        if(this.volume < other.volume)
+            return -1;
+        else if(this.volume > other.volume)
+            return 1;
+        else
+            return 0;
+    }
+}
+
+/*Box라는 이름으로 클래스를 생성하고 실수형으로 필드(volume)를 하나 가진다.
+실행클래스를 생성하여 두개의 객체를 생성하여 두 객체의 필드 값을 비교하여 큰지 작은지 같은지를 판별하도록 코드를 작성하여 보자.*/
+public class BoxTest {
+    public static void main(String[] args) {
+        Box b1 = new Box(100.4);
+        Box b2 = new Box(85.0);
+
+        if(b1.getVolume() > b2.getVolume())
+            System.out.println("b1이 b2보다 더 크다");
+        else
+            System.out.println("b1이 b2보다 같거나 작다");
+
+        if(b1.compareTo(b2) > 0)
+            System.out.println("b1이 b2보다 더 크다");
+        else
+            System.out.println("b1이 b2보다 같거나 작다");
+    }
+}
---
+++ java_project_day7/exam_interface/StudentSort.java
@@ -0,0 +1,65 @@
+package exam_interface;
+
+import java.util.Arrays;
+
+/* Arrays 클래스의 sort()메소드는 대상 객체가
+ * Comparable 인터페이스를 구현하고 있는 경우에만 정렬 서비스를 제공한다.
+ * Student 클래스를 작성하고 학생들의 평점을 기준으로 정렬하는 예제이다.
+ *
+ * Student 클래스를 생성하고 이름(name)과 평점(gpa)을 필드로 가진다.
+ * 매개변수를 가진 생성자, 접근자, toString()을 생성한다.
+ */
+@SuppressWarnings("rawtypes")
+class Student implements Comparable{
+    private String name; // 이름
+    private double gpa; // 평점
+
+    public Student(String name, double gpa){
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String getName(){ return name; }
+    public double getGpa(){ return gpa; }
+
+    @Override
+    public String toString(){
+        return "이름="+name+" 평점="+gpa;
+    }
+
+    // Comparable의 추상메서드 구현
+    public int compareTo(Object obj){ // 슈퍼클래스 참조변수 = 서브클래스의 참조값. 업캐스팅
+        Student other = (Student)obj; // 서브클래스 참조변수 = (서브클래스)슈퍼클래스의 타입 참조변수. 다운캐스팅
+        // 평점 기준
+        if(this.gpa < other.gpa)
+            return -1;
+        else if(this.gpa > other.gpa)
+            return 1;
+        else
+            return 0;
+    }
+}
+
+
+public class StudentSort {
+    public static void main(String[] args) {
+        // 5개의 숫자를 오름차순 정렬하여 출력하는 코드를 작성해주세요.
+        int[] numbers = {60, 80, 10, 45, 90};
+        Arrays.sort(numbers);
+
+        for(int num : numbers)
+            System.out.print(num+" ");
+        System.out.println("\n"); // System.out.println();와 다른 점?
+
+        // 3명의 학생 평점으로 오름차순 정렬하여 출력하는 코드를 작성해 주세요.
+        Student[] students = new Student[3];
+        students[0] = new Student("홍길동", 3.39);
+        students[1] = new Student("이한솔", 4.21);
+        students[2] = new Student("김희진", 2.19);
+        Arrays.sort(students);
+
+        for(Student s : students)
+            System.out.println(s);
+    }
+}
+
