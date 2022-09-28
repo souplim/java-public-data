af22fca0a0c51946a1336af9fbc4814aa6d2c0cb
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-28 09:53:45 +0900

    Update BoxTest.java
    부가설명 추가

--- java_project_day7/exam_interface/BoxTest.java
+++ java_project_day7/exam_interface/BoxTest.java
@@ -1,6 +1,6 @@
 package exam_interface;
 
-/* 자바에서는 객체의 크기를 비교하는데 사용되는 Comparable 인터페이스를 제공해 준다.
+/* 자바에서는 객체의 크기를 비교하는데 사용되는 Comparable 인터페이스를 제공해 준다(자바 API가 제공하는 인터페이스).
  * public interface Comparable {
        // 이 객체가 다른 객체보다 크면 1, 같으면 0, 작으면 -1을 반환한다.
        int compareTo(Object other);
@@ -19,8 +19,8 @@ class Box implements Comparable{
 
     @Override
     public int compareTo(Object obj){ // Object obj = b2 업캐스팅
-        Box other = (Box)obj; // (서브클래스명)부모클래스의 참조변수. 다운캐스팅
-        if(this.volume < other.volume)
+        Box other = (Box)obj; // (서브클래스명)부모클래스의 참조변수. 다운캐스팅. 자식변수가 필요하기 때문에 형변환하면 접근할 수 있는 범위 확대됨
+        if(this.volume < other.volume) // this -> b1이 가지고 있는 볼륨 other -> b2가 가지고 있는 볼륨
             return -1;
         else if(this.volume > other.volume)
             return 1;
