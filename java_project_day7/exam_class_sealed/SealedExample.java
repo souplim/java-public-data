72a86ae1d19da617735feaff4204ff07479e9592
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-09-28 16:52:22 +0900

    Update SealedExample.java

--- java_project_day7/exam_class_sealed/SealedExample.java
+++ java_project_day7/exam_class_sealed/SealedExample.java
@@ -17,15 +17,42 @@ final class Employee extends Person{
     }
 }
 
-public non-sealed class Manager extends Person{
+non-sealed class Manager extends Person{
     @Override
     public void work(){
         System.out.println("생산 관리를 합니다.");
     }
 }
 
+class Director extends Manager{
+    @Override
+    public void work(){
+        System.out.println("제품을 기획합니다.");
+    }
+}
+
+
 public class SealedExample {
     public static void main(String[] args){
+        Person p = new Person();
+        Employee e = new Employee();
+        Manager m = new Manager();
+        Director d = new Director();
 
+        p.work();
+        e.work();
+        m.work();
+        d.work();
+
+        // 위 내용을 객체배열로 정의
+        Person[] p1 = {
+                new Person(),
+                new Employee(),
+                new Manager(),
+                new Director()
+        };
+
+        for(Person per : p1)
+           per.work();
     }
 }
