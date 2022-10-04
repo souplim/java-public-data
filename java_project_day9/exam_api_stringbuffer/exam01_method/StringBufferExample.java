629abbf4099e71cb0d5a3c32d613f7d92d745310
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-10-04 17:09:34 +0900

    Update StringBufferExample.java

--- java_project_day9/exam_api_stringbuffer/exam01_method/StringBufferExample.java
+++ java_project_day9/exam_api_stringbuffer/exam01_method/StringBufferExample.java
@@ -4,13 +4,25 @@ public class StringBufferExample {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("This");
 
-        sb.append(" is pencil"); // 문자열 덧붙이기
+        sb.append(" is pencil"); // 문자열 덧붙이기 This is pencil
         System.out.println(sb);
 
-        sb.insert(7, " my"); // 문자열 삽입
+        sb.insert(7, " my"); // 문자열 삽입 This is my pencil
         System.out.println(sb);
 
-        sb.replace(8, 10, "your"); // 문자열 대체
+        sb.replace(8, 10, "your"); // 문자열 대체 This is your pencil
         System.out.println(sb);
+
+        sb.delete(8, 13); // This is pencil
+        System.out.println(sb);
+
+        sb.setLength(4); // 길이 변경 This
+        System.out.println(sb);
+
+        sb.reverse(); // sihT
+        System.out.println(sb);
+
+        sb.delete(0, sb.length());
+        System.out.println(sb.toString().isEmpty()); // true
     }
 }
