a9ee37762c59dd28d461881a5838f713c8c7af1b
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-10-07 17:50:32 +0900

    Update ArrayListTest02.java

--- java_project_day10/exam_collection_list/ArrayListTest02.java
+++ java_project_day10/exam_collection_list/ArrayListTest02.java
@@ -56,14 +56,48 @@ public class ArrayListTest02 {
         // 8 : [서울, 북경, 서울, 도쿄, 뉴욕, 런던, 로마, 방콕, 북경, 도쿄, 서울]
         Object obj[] = list.toArray();
         System.out.println("8 : "+ Arrays.toString(obj));
-        // <T> T
+        // <T> T[] toArray(T[] a) : 제네릭 메서드로 T 타입의 배열 생성
         // 9 : [서울, 북경, 서울, 도쿄, 뉴욕, 런던, 로마, 방콕, 북경, 도쿄, 서울]
+        String cities[] = new String[0];
+//        cities = list.<String>toArray(cities);
+        cities = list.toArray(cities);
+        System.out.println("9 : "+Arrays.toString(cities)+""+cities.length);
         // 10 : []
+        list.clear();
+        print(10, list);
         // 11 : true
+        System.out.println(list.isEmpty());
+
+        list.add("파리");
+        list.add("방콕");
+        list.add("LA");
+        // Arrays 클래스의 static <T> List<T> asList(T...a) : 제네릭 메서드
         // 12 : [서울, 뉴욕, 상해]
+//        List<String> list2 = Arrays.asList("서울","뉴욕","상해");
+        // 배열 -> List<E> 타입으로 변환
+        String[] names = new String[]{"서울","뉴욕","상해"};
+        List<String> list2 = Arrays.asList(names);
+        print(12, list2);
+
+        List<String> list3 = Arrays.asList("서울","부산","상해");
+
+        // boolean addAll(Collections<? extends E> c)
         // 13 : [파리, 방콕, LA, 서울, 뉴욕, 상해]
+        list.addAll(list2);
+        print(13, list);
+
+        // boolean containAll(Collections<?> c)
         // 14 : false
+        System.out.println("14 : "+list.containsAll(list3));
+
+        // boolean retainAll(Collections<?> c) list에서 list2의 데이터만 남겨두고 나머지 데이터는 모두 삭제한다.
         // 15 : [서울, 뉴욕, 상해]
+        list.retainAll(list2);
+        print(15, list);
+
+        // boolean removeAll(Collection<?> c)
         // 16 : []
+        list.removeAll(list2);
+        print(16, list);
     }
 }
