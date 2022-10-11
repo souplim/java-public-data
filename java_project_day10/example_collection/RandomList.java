26353e1a211b81dc8606dc3bfafe8db54951ddf0
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-10-11 11:55:22 +0900

    Update RandomList.java
    Random 클래스 이용하여 난수 발생, Iterator 사용하여 출력 코드 추가

--- java_project_day10/example_collection/RandomList.java
+++ java_project_day10/example_collection/RandomList.java
@@ -1,26 +1,26 @@
 package example_collection;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /* [요구사항] 1~100사이의 임의의 숫자 10개를 출력하고 오름차순(낮은 값에서 높은 값)으로 정렬하여 출력해 주세요. */
 public class RandomList {
     public static void main(String[] args) {
+//        Random r = new Random();
         List<Integer> list = new ArrayList<>();
 
         final int NUM = 10;
 
         for(int i=0; i<NUM; i++){
             list.add((int)(Math.random()*100)+1);
+//            list.add(r.nextInt(100)+1);
         }
 
         System.out.println("정렬 전 list 데이터");
-        for(Integer number : list)
-            System.out.print(number+" ");
-        System.out.println();
+        Iterator<Integer> it = list.iterator();
+        while(it.hasNext())
+            System.out.print(it.next()+"\t");
 
-        System.out.println("정렬 후 list 데이터");
+        System.out.println("\n정렬 후 list 데이터");
         Collections.sort(list);
         for(Integer number : list)
             System.out.print(number+" ");
