514c0a53fdb10c8ce60cdbf140f4f7f5d4bc27dc
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-10-11 15:09:58 +0900

    Add files via upload

---
+++ java_project_day10/example_collection/WordFreq.java
@@ -0,0 +1,11 @@
+package example_collection;
+
+/*[요구사항] 문자열에 포함된 단어의 빈도를 계산하여 단어를 Key로, 빈도를 Value로 저장하여 출력해 보세요.
+ * String[] sample = { "to", "be", "or", "not", "to", "be", "is", "a", "problem"};
+ * [실행결과]
+   총 단어 수 : 7개
+   {a=1, not=1, be=2, or=1, problem=1, is=1, to=2}
+*/
+public class WordFreq {
+
+}
