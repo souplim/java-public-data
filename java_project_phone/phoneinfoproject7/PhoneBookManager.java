efb631fb17e2367367a588e741beca759119242e
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-10-12 15:21:09 +0900

    Add files via upload

---
+++ java_project_phone/phoneinfoproject7/PhoneBookManager.java
@@ -0,0 +1,170 @@
+package phoneinfoproject7;
+
+import phoneinfoproject7.PhoneInfo;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class PhoneBookManager implements Comparable<PhoneBookManager> {
+    private static final int size=100;
+//    PhoneInfo[] infoStorage; // 전화번호 저장하는 객체배열
+    HashSet<phoneinfoproject7.PhoneInfo> infoStorage; // 전화번호 저장하는 HashSet<>
+    int type;
+    String name, phone, major, company = null;
+    int year;
+    private int cnt = 0; // 현재 저장된 객체들의 인덱스
+
+    public PhoneBookManager(){
+//        infoStorage = new PhoneInfo[size];
+        infoStorage = new HashSet<>();
+    }
+
+    public void input() throws MenuChoiceException {
+        System.out.println("데이터 입력을 시작합니다.");
+        System.out.print("1.일반 2.대학 3.회사\n선택 >> ");
+
+        type = MenuViewer.sc.nextInt();
+        MenuViewer.sc.nextLine();
+        PhoneInfo info = null;
+
+        switch(type){
+            case 1:
+                info = readInfo();
+                infoStorage.add(info);
+                break;
+            case 2:
+                info = readUnivInfo();
+                infoStorage.add(info);
+                break;
+            case 3:
+                info = readCompanyInfo();
+                infoStorage.add(info);
+                break;
+            default :
+                System.out.println("잘못 입력 하셨습니다.");
+                return;
+        }
+        System.out.println("데이터 입력이 완료되었습니다.");
+    }
+
+    private PhoneInfo readInfo(){
+        System.out.print("이름 : ");
+        name = MenuViewer.sc.nextLine();
+        System.out.print("전화번호 : ");
+        phone = MenuViewer.sc.nextLine();
+
+        return new PhoneInfo(name, phone);
+    }
+
+    public PhoneInfo readUnivInfo(){
+        System.out.print("이름 : ");
+        name = MenuViewer.sc.nextLine();
+        System.out.print("전화번호 : ");
+        phone = MenuViewer.sc.nextLine();
+        System.out.print("전공 : ");
+        major = MenuViewer.sc.nextLine();
+        System.out.print("학년 : ");
+        year = MenuViewer.sc.nextInt();
+
+        return new PhoneUnivInfo(name, phone, major, year);
+    }
+
+    public PhoneInfo readCompanyInfo(){
+        System.out.print("이름 : ");
+        name = MenuViewer.sc.nextLine();
+        System.out.print("전화번호 : ");
+        phone = MenuViewer.sc.nextLine();
+        System.out.print("회사 : ");
+        company = MenuViewer.sc.nextLine();
+        System.out.println("데이터 입력이 완료되었습니다.");
+
+        return new PhoneCompanyInfo(name, phone, company);
+    }
+
+//    public void search() { // 인덱스 번호 반환하는 메서드 따로 떼기
+//        System.out.println("데이터 검색을 시작합니다.");
+//        System.out.print("이름 : ");
+//        name = MenuViewer.sc.nextLine();
+//
+//        int dataIdx = searchName(name);
+//        if(dataIdx<0)
+//            System.out.println("해당하는 데이터가 존재하지 않습니다. \n");
+//        else{
+//            infoStorage[dataIdx].showPhoneInfo();
+//            System.out.println("데이터 검색이 완료되었습니다. \n");
+//        }
+//    }
+
+//    private int searchName(String name){ // 내부에서만 사용될 메서드 private
+//        for(int i=0; i<cnt; i++){
+//            PhoneInfo curInfo = infoStorage[i];
+//            // compareTo() 객체의 필드값을 비교할 때 사용하는 메서드
+//            // 비교대상인 값.compareTo(비교할 값)이 일치하면 0을 리턴
+//            // 두 개의 값이 일치하지 않을시 0외의 값인 1, -1을 리턴
+//            if(name.compareTo(curInfo.getName())==0)
+//                return i;
+//        }
+//        return -1;
+//    }
+
+    public void search() { // 인덱스 번호 반환하는 메서드 따로 떼기
+        System.out.println("데이터 검색을 시작합니다.");
+        System.out.print("이름 : ");
+        name = MenuViewer.sc.nextLine();
+
+        if(searchName(name)==null)
+            System.out.println("해당하는 데이터가 존재하지 않습니다. \n");
+        else{
+            info.showPhoneInfo();
+            System.out.println("데이터 검색이 완료되었습니다. \n");
+        }
+    }
+
+    private String searchName(String name){ // 내부에서만 사용될 메서드 private
+        Iterator<PhoneInfo> it = infoStorage.iterator();
+        while(it.hasNext()){
+            String n = it.next().getName();
+            if(name.compareTo(n)==0)
+                return name;
+        }
+        return null;
+    }
+
+    public void delete() {
+        System.out.println("데이터 삭제를 시작합니다.");
+        System.out.print("이름 : ");
+        name = MenuViewer.sc.nextLine();
+
+        int dataIdx = searchName(name); // 그 사용자의 인덱스를 불러온다
+        if(dataIdx<0)
+            System.out.println("해당하는 데이터가 존재하지 않습니다. \n");
+        else {
+            for(int i=dataIdx; i<cnt-1; i++)
+                infoStorage[i]=infoStorage[i+1];
+            infoStorage[cnt-1] = null;
+            cnt--;
+            System.out.println("데이터 삭제가 완료되었습니다.");
+        }
+    }
+
+    @Override
+    public int compareTo(PhoneBookManager pm){
+        return this.name.compareTo(pm.name);
+    }
+}
+
+class MenuViewer {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void intro() { // 메뉴 뷰어는 객체 생성 않고 사용할 수 있게 static으로 선언할 수 있음
+        System.out.println();
+        System.out.println("선택하세요");
+        System.out.println("1. 데이터 입력");
+        System.out.println("2. 데이터 검색");
+        System.out.println("3. 데이터 삭제");
+        System.out.println("4. 프로그램 종료");
+        System.out.print("선택 : ");
+    }
+}
---
+++ java_project_phone/phoneinfoproject7/PhoneBookVer07.java
@@ -0,0 +1,132 @@
+package phoneinfoproject7;
+
+import phoneinfoproject.PhoneBookManager;
+
+public class PhoneBookVer07 {
+    public static void main(String[] args) {
+        PhoneBookManager pbm = new PhoneBookManager();
+        int menu;
+
+        while (true) {
+            try {
+                MenuViewer.intro();
+                menu = MenuViewer.sc.nextInt();
+                MenuViewer.sc.nextLine();
+
+                if (menu < 1 || 3 < menu) {
+                    throw new MenuChoiceException(menu);
+                }
+
+                switch (menu) {
+                    case 1:
+                        pbm.input();
+                        break;
+                    case 2:
+                        pbm.search();
+                        break;
+                    case 3:
+                        pbm.delete();
+                        break;
+                    case 4:
+                        System.out.println("프로그램을 종료합니다.");
+                        return;
+                }
+            } catch (MenuChoiceException e) {
+                e.showWrongChoice();
+                System.out.println("메뉴 선택을 처음부터 다시 진행합니다.");
+            }
+        }
+    }
+}
+
+class PhoneInfo {
+    private String name;
+    private String phoneNumber;
+
+    PhoneInfo(){ }
+    PhoneInfo(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setName(String name){ this.name = name; }
+    public String getName(){ return name; }
+    public void setPhoneNumber(String phoneNumber){ this.phoneNumber = phoneNumber; }
+    public String getPhoneNumber(){ return phoneNumber; }
+
+    public void showPhoneInfo(){
+        System.out.println("name : "+name);
+        System.out.println("phone : "+phoneNumber);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj instanceof PhoneInfo){
+            PhoneInfo p = (PhoneInfo)obj;
+            if(this.name.equals(p.name))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        return name.hashCode(); // String 클래스의 hashCode()
+    }
+}
+
+class PhoneUnivInfo extends PhoneInfo{ // 대학동기들
+    private String major;
+    private int year;
+
+    public PhoneUnivInfo(){ }
+    public PhoneUnivInfo(String name, String phoneNumber, String major, int year){
+        super(name, phoneNumber);
+        this.major = major;
+        this.year = year;
+    }
+
+    public String getMajor() { return major; }
+    public void setMajor(String major) { this.major = major; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+
+    @Override
+    public void showPhoneInfo(){
+        super.showPhoneInfo();
+        System.out.println("major : "+getMajor());
+        System.out.println("year : "+getYear());
+    }
+}
+
+class PhoneCompanyInfo extends PhoneInfo{ // 회사동료
+    private String company;
+
+    public PhoneCompanyInfo(){ }
+    public PhoneCompanyInfo(String name, String phoneNumber, String company){
+        super(name, phoneNumber);
+        this.company = company;
+    }
+
+    public String getCompany() { return company; }
+    public void setCompany(String company) { this.company = company; }
+
+    @Override
+    public void showPhoneInfo(){
+        super.showPhoneInfo();
+        System.out.println("company : "+getCompany());
+    }
+}
+
+class MenuChoiceException extends Exception {
+    private int wrongChoice;
+
+    public MenuChoiceException(int choice){
+        super("잘못된 선택이 이뤄졌습니다.");
+        wrongChoice = choice;
+    }
+
+    public void showWrongChoice(){
+        System.out.println(wrongChoice+"에 해당하는 선택은 존재하지 않습니다.");
+    }
+}
