f3a588bbc3eda94921ca25e918b10028ac08d284
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-10-19 14:09:01 +0900

    Update LineTest.java
    형식자 출력으로 수정

--- java_project_day11/exam_class/LineTest.java
+++ java_project_day11/exam_class/LineTest.java
@@ -4,18 +4,20 @@ import java.io.*;
 
 public class LineTest {
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(new FileReader("writer.txt"));
-            BufferedWriter bw = new BufferedWriter(new FileWriter("line.txt"))){
+        try( BufferedReader br = new BufferedReader(new FileReader("writer.txt"));
+             PrintWriter pw = new PrintWriter(new FileWriter("line.txt"))){
 
-        String readTxt = "";
-        int i = 1;
+            String readTxt = "";
+            int i = 1;
 
-        while((readTxt = br.readLine()) != null){
-            bw.write(i+": ");
-            bw.write(readTxt+"\n");
-            i++;
-        }
+            while((readTxt = br.readLine()) != null){
+                pw.format("%6d: %s\n", i, readTxt);
+                i++;
+            }
 
+            pw.flush();
+            br.close();
+            pw.close();
         } catch(IOException io){
             io.printStackTrace();
         }
