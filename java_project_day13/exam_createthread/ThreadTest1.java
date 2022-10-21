678bd223a4430bc08ca73861c70f3632b027a419
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-10-21 09:41:17 +0900

    Update ThreadTest1.java
    스레드 생성방법 추가

--- java_project_day13/exam_createthread/ThreadTest1.java
+++ java_project_day13/exam_createthread/ThreadTest1.java
@@ -15,16 +15,28 @@ class MyRunnable implements Runnable {
 
 public class ThreadTest1 {
     public static void main(String[] args) {
+        // 1
         MyRunnable r1 = new MyRunnable("First Thread");
         Thread t1 = new Thread(r1);
-        Thread t2 = new Thread(new MyRunnable("Second Thread"));
-
         t1.start();
+
+        // 2
+        Thread t2 = new Thread(new MyRunnable("Second Thread"));
         t2.start();
 
+        // 3
+        MyRunnable r = () -> {
+            public void run(){
+                for(int i=0;i<10;i++)
+                    System.out.printf("%-13s = %d\n","First Thread", i);
+            }
+        };
+        (new Thread(r)).start();
+
+        // 4
         (new Thread(() -> {
             for(int i=0;i<10;i++)
-                System.out.printf("%-13s = %d\n","Thread", i);
+                System.out.printf("%-13s = %d\n","Second Thread", i);
         })).start();
     }
 }
