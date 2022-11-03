6aed733cef07931ce1370ceed4b8dd888568e977
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-11-03 11:17:28 +0900

    Update PDB_JAVAUSER.sql

--- db_project_day1/PDB_JAVAUSER.sql
+++ db_project_day1/PDB_JAVAUSER.sql
@@ -1,7 +1,7 @@
---DROP TABLE SUBJECT;
---DROP TABLE STUDENT;
---DROP TABLE LESSON;
-
+DROP TABLE SUBJECT;
+DROP TABLE STUDENT;
+DROP TABLE LESSON;
+DROP TABLE TRAINEE;
 -- 학과테이블 생성
 CREATE TABLE SUBJECT(
     NO NUMBER(4) NOT NULL,                       -- 일련번호. 자리수 생각하면 NUMBER가 가지고 있는 자리수 만큼 다 사용 가능
@@ -28,7 +28,7 @@ CREATE TABLE STUDENT(
     SD_NAME VARCHAR2(12) NOT NULL,          -- 이름
     SD_ID VARCHAR2(10) NOT NULL,            -- 아이디
     SD_PASSWD VARCHAR2(20) NOT NULL,        -- 비밀번호
-    S_NUM CHAR(2) NOT NULL,                 -- 학과번호
+    S_NUM VARCHAR(2) NOT NULL,              -- 학과번호(학과테이블)
     SD_BIRTH CHAR(8) NOT NULL,              -- 생년월일
     SD_PHONE CHAR(13) NOT NULL,             -- 핸드폰
     SD_ADDRESS VARCHAR2(20 CHAR) NOT NULL,  -- 주소
@@ -42,26 +42,25 @@ CREATE TABLE STUDENT(
 );
 
 -- 학생테이블 데이터 입력
-INSERT INTO STUDENT
-VALUES(1,06010001,'김정수','javajsp','1234','01','19920514','010-1234-1234','서울시 서대문구 창전동','kjs@gmail.com',SYSDATE);
-INSERT INTO STUDENT
-VALUES(2,95010002,'김수현','jdbcmania','4321','01','19840403','010-1234-1234','서울시 서초구 양재동','ksh@gmail.com',SYSDATE);
-INSERT INTO STUDENT
-VALUES(3,98040001,'공지영','gonji','1111','04','19870506','010-1234-1234','부산광역시 해운대구 반송동','kjy@gmail.com',SYSDATE);
-INSERT INTO STUDENT
-VALUES(4,02050001,'조수영','water','9987','05','19801213','010-1234-1234','대전광역시 중구 은행동','jsy@gmail.com',SYSDATE);
-INSERT INTO STUDENT
-VALUES(5,94040002,'최경란','novel','8765','04','19760708','010-1234-1234','경기도 수원시 장안구 이목동','ckr@gmail.com',SYSDATE);
-INSERT INTO STUDENT
-VALUES(6,08020001,'안익태','korea','4565','02','19881104','010-1234-1234','서울시 역삼동','ait@gmail.com',SYSDATE);
-SELECT * FROM STUDENT;
+INSERT INTO STUDENT(NO, SD_NUM, SD_NAME, SD_ID, SD_PASSWD, S_NUM, SD_BIRTH, SD_PHONE, SD_ADDRESS, SD_EMAIL)
+VALUES(1,06010001,'김정수','javajsp','1234','01','19920514','010-1234-1234','서울시 서대문구 창전동','kjs@gmail.com');
+INSERT INTO STUDENT(NO, SD_NUM, SD_NAME, SD_ID, SD_PASSWD, S_NUM, SD_BIRTH, SD_PHONE, SD_ADDRESS, SD_EMAIL)
+VALUES(2,95010002,'김수현','jdbcmania','4321','01','19840403','010-1234-1234','서울시 서초구 양재동','ksh@gmail.com');
+INSERT INTO STUDENT(NO, SD_NUM, SD_NAME, SD_ID, SD_PASSWD, S_NUM, SD_BIRTH, SD_PHONE, SD_ADDRESS, SD_EMAIL)
+VALUES(3,98040001,'공지영','gonji','1111','04','19870506','010-1234-1234','부산광역시 해운대구 반송동','kjy@gmail.com');
+INSERT INTO STUDENT(NO, SD_NUM, SD_NAME, SD_ID, SD_PASSWD, S_NUM, SD_BIRTH, SD_PHONE, SD_ADDRESS, SD_EMAIL)
+VALUES(4,02050001,'조수영','water','9987','05','19801213','010-1234-1234','대전광역시 중구 은행동','jsy@gmail.com');
+INSERT INTO STUDENT(NO, SD_NUM, SD_NAME, SD_ID, SD_PASSWD, S_NUM, SD_BIRTH, SD_PHONE, SD_ADDRESS, SD_EMAIL)
+VALUES(5,94040002,'최경란','novel','8765','04','19760708','010-1234-1234','경기도 수원시 장안구 이목동','ckr@gmail.com');
+INSERT INTO STUDENT(NO, SD_NUM, SD_NAME, SD_ID, SD_PASSWD, S_NUM, SD_BIRTH, SD_PHONE, SD_ADDRESS, SD_EMAIL)
+VALUES(6,08020001,'안익태','korea','4565','02','19881104','010-1234-1234','서울시 역삼동','ait@gmail.com');
 
 SELECT * FROM STUDENT;
 
 -- 과목테이블 생성
 CREATE TABLE LESSON(
     NO NUMBER(4) NOT NULL,              -- 일련번호
-    L_ABBRE VARCHAR2(20) NOT NULL,      -- 과목약어
+    L_ABBRE VARCHAR2(2) NOT NULL,       -- 과목약어
     L_NAME VARCHAR2(20) NOT NULL,       -- 과목명
     
     CONSTRAINT LESSON_NO_PK PRIMARY KEY(NO),
@@ -69,21 +68,21 @@ CREATE TABLE LESSON(
 );
 
 -- 과목테이블 데이터 입력
-INSERT INTO LESSON VALUES(1,'K','국어');
-INSERT INTO LESSON VALUES(2,'M','수학');
-INSERT INTO LESSON VALUES(3,'E','영어');
-INSERT INTO LESSON VALUES(4,'H','역사');
-INSERT INTO LESSON VALUES(5,'P','프로그래밍');
-INSERT INTO LESSON VALUES(6,'D','데이터베이스');
-INSERT INTO LESSON VALUES(7,'ED','교육학이론');
+INSERT INTO LESSON(NO, L_ABBRE, L_NAME) VALUES(1,'K','국어');
+INSERT INTO LESSON(NO, L_ABBRE, L_NAME) VALUES(2,'M','수학');
+INSERT INTO LESSON(NO, L_ABBRE, L_NAME) VALUES(3,'E','영어');
+INSERT INTO LESSON(NO, L_ABBRE, L_NAME) VALUES(4,'H','역사');
+INSERT INTO LESSON(NO, L_ABBRE, L_NAME) VALUES(5,'P','프로그래밍');
+INSERT INTO LESSON(NO, L_ABBRE, L_NAME) VALUES(6,'D','데이터베이스');
+INSERT INTO LESSON(NO, L_ABBRE, L_NAME) VALUES(7,'ED','교육학이론');
 
 SELECT * FROM LESSON;
 
 -- 수강테이블 생성
 CREATE TABLE TRAINEE(
     NO NUMBER(4) PRIMARY KEY,                                               -- 일련번호
-    SD_NUM CHAR(8) NOT NULL,                                                -- 학번
-    L_ABBRE VARCHAR2(20) NOT NULL,                                          -- 과목약어
+    SD_NUM VARCHAR(8) NOT NULL,                                             -- 학번(학생테이블)
+    L_ABBRE VARCHAR2(2) NOT NULL,                                           -- 과목약어(과목테이블)
     T_SECTION VARCHAR2(10) CHECK(T_SECTION IN('CULTURE','MAJOR','MINOR')),  -- 과목구분
     T_DATE DATE DEFAULT SYSDATE,                                            -- 등록일자
     
@@ -92,7 +91,7 @@ CREATE TABLE TRAINEE(
 );
 
 -- 수강테이블 데이터 입력
-INSERT INTO TRAINEE VALUES(1, 06010001, 'K', 'CULTURE', SYSDATE);
-INSERT INTO TRAINEE VALUES(2, 95010002, 'E', 'MAJOR', SYSDATE);
+INSERT INTO TRAINEE(NO, SD_NUM, L_ABBRE, T_SECTION) VALUES(1, 06010001, 'K', 'CULTURE');
+INSERT INTO TRAINEE(NO, SD_NUM, L_ABBRE, T_SECTION) VALUES(2, 95010002, 'E', 'MAJOR');
 
 SELECT * FROM TRAINEE;
