8336afefa5b80edc94ed361eaaa4077a82d9fc4e
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-11-21 15:18:35 +0900

    Update AcademicManagement.java

--- java_project_day15/academic_management/AcademicManagement.java
+++ java_project_day15/academic_management/AcademicManagement.java
@@ -1,11 +1,13 @@
 package academic_management;
 
+import student_management.StudentManagement;
 import subject_management.SubjectManagement;
 
 // 실행 클래스
 public class AcademicManagement {
     public static void main(String[] args) {
         SubjectManagement subject = new SubjectManagement();
+        StudentManagement student = new StudentManagement();
         int topMenuChoice, subMenuChoice;
 
         System.out.println("학사 관리 프로그램을 시작합니다");
@@ -49,7 +51,24 @@ public class AcademicManagement {
                         System.out.println("조회, 입력, 수정, 삭제, 검색 중에 하나를 선택해주세요");
                 }
             } else if (topMenuChoice == 2){
-                System.out.println("준비중");
+                switch(subMenuChoice){
+                    case 1:
+                        student.read("select");
+                        break;
+                    case 2:
+                        student.create();
+                        break;
+                    case 3:
+                        student.update();
+                        break;
+                    case 4:
+                        student.delete();
+                        break;
+                    case 5:
+                        student.read("search");
+                        break;
+                    default :
+                        System.out.println("조회, 입력, 수정, 삭제, 검색 중에 하나를 선택해주세요");
             } else {
                 System.out.println("프로그램 종료");
                 System.exit(0);
