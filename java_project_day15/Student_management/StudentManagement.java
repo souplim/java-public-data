4be141d24ce66b61b6000c985326a9c76b889719
yoonjae <107927616+eunja2@users.noreply.github.com>
2022-11-22 21:08:52 +0900

    Update StudentManagement.java

--- java_project_day15/Student_management/StudentManagement.java
+++ java_project_day15/Student_management/StudentManagement.java
@@ -1,6 +1,9 @@
 package student_management;
 
 import academic_management.MenuViewer;
+import subject_management.SubjectVO;
+
+import java.util.ArrayList;
 
 // 학과 조회, 입력, 수정, 삭제, 검색 등의 실질적인 기능을 구현해놓은 클래스
 public class StudentManagement {
@@ -9,8 +12,20 @@ public class StudentManagement {
     public void read(String mode){
         StudentVO vo = null; // 레코드 가져오기 위한 인스턴스
         if(mode.equals("search")){
-//            vo = inputData("search");
-            System.out.println("검생 이름 :"+vo.getSd_num());
+            vo = inputData("search");
+            System.out.println("검색 학번 : "+vo.getSd_num());
+        }
+        ArrayList<StudentVO> svo = dao.getStudentTotal(vo);
+
+        System.out.println("\n**** student 테이블 데이터 출력 ****");
+        System.out.println("번호\t학번\t이름\t아이디\t패스워드\t학과번호\t생년월일\t핸드폰번호\t주소\t이메일주소\t등록일자");
+        if(svo.size()>0){
+//            for(int i=0; i<svo.size(); i++){
+//                StudentVO stu = svo.get(i);
+            for(StudentVO stu : svo)
+                System.out.print(stu.getNo()+"\t"+stu.getSd_num()+"\t"+stu.getSd_name()+"\t"+stu.getSd_id()+"\t"+stu.getSd_passwd()+"\t"+stu.getS_num()+"\t"+stu.getSd_birth()+"\t"+stu.getSd_phone()+"\t"+stu.getSd_address()+"\t"+stu.getSd_email()+"\t"+stu.getSd_date()+"\n");
+        } else {
+            System.out.println("학생 정보가 존재하지 않습니다.");
         }
     }
 
@@ -19,10 +34,6 @@ public class StudentManagement {
         String sd_num = null, sd_name = null, sd_id = null, sd_passwd = null, s_num = null, sd_birth = null, sd_phone = null, sd_address = null, sd_email = null;
 
         if(mode.equals("input")){
-            System.out.print("학번 입력 : ");
-            sd_num = dao.getStudentNum(); // 자동으로 학번 부여
-            System.out.println(sd_num);
-
             System.out.print("이름 입력 : ");
             sd_name = MenuViewer.keyboard.nextLine();
             System.out.print("아이디 입력 : ");
@@ -39,21 +50,30 @@ public class StudentManagement {
             sd_address = MenuViewer.keyboard.nextLine();
             System.out.print("이메일 주소 입력 : ");
             sd_email = MenuViewer.keyboard.nextLine();
+            System.out.print("학번 : ");
+//            sd_num = dao.getStudentNum(); // 자동으로 학번 부여 --?
+//            sd_num = dao.getStudentNum(no, s_num, sd_birth); -- no를 여기서 부여?
+            System.out.println(sd_num);
         }
 
-        if(!mode.equals("delete")){ // 수정할 내용
+        if(mode.equals("search")){
             System.out.print("학번 입력 : ");
             sd_num = MenuViewer.keyboard.nextLine();
         }
 
+        if(mode.equals("delete")){
+            System.out.println("삭제할 학생의 학번을 입력해주세요.");
+            System.out.print("학번 : ");
+            sd_num = MenuViewer.keyboard.nextLine();
+        }
         StudentVO sub = new StudentVO(sd_num, sd_name, sd_id, sd_passwd, s_num, sd_birth, sd_phone, sd_address, sd_email);
         return sub;
     }
 
-    private int inputDataNo(){
-        int sd_num;
+    private String inputDataNum(){
+        String sd_num;
         System.out.print("학번 입력 : "); // 해당 학번을 가진 학생의 정보 수정
-        sd_num = MenuViewer.keyboard.nextInt();
+        sd_num = MenuViewer.keyboard.nextLine();
         return sd_num;
     }
 
@@ -68,12 +88,26 @@ public class StudentManagement {
     public void update() {
         StudentVO svo = inputData("update");
 
-        int no = inputDataNo(); // 번호
-        svo.setNo(no);
+        String sd_num = inputDataNum(); // 학번 받아서 그 학번 학생 정보를 수정하게 함
+        svo.setSd_num(sd_num);
 
         if(dao.studentUpdate(svo))
             System.out.println("학과 데이터 수정 성공");
         else
             System.out.println("학과 데이터 수정 실패");
+    }
+
+    public void delete() {
+        StudentVO svo = inputData("delete");
+
+        String sd_num = inputDataNum();
+        StudentVO sv = new StudentVO();
+        sv.setSd_num(sd_num);
+
+        if(dao.studentDelete(sv))
+            System.out.println("학과 데이터 삭제 성공");
+        else
+            System.out.println("학과 데이터 삭제 실패");
+
     }
 }
