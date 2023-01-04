8eaa74f1683494262f74e7e2a1d6b3eea06a9dd0
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-01-04 15:19:13 +0900

    Add files via upload

---
+++ JSPCoding/firstExample/src/main/java/example/Clock.java
@@ -0,0 +1,16 @@
+package example;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/* 웹에서 클래스를 접근하기 위해서는 반드시 패키지 설정이 선행되어야 한다. */
+
+public class Clock {
+	
+	public String now() {
+		Date nowDate = new Date();
+		SimpleDateFormat timeFormat = new SimpleDateFormat("yyy년 MM월 dd일 hh시 mm분 ss초");
+		String formatTime = timeFormat.format(nowDate);
+		
+		return formatTime;
+	}
+}
