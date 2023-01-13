7cb3463f9017016df843eed6e5172ab45504f05f
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-01-13 09:06:15 +0900

    Update ChungnamServlet.java

--- JSPCoding/servletExample/src/main/java/com/jdbc/ChungnamServlet.java
+++ JSPCoding/servletExample/src/main/java/com/jdbc/ChungnamServlet.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
