e247b1e4a0efef01e618d6bd42f3c6e6267ff952
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-01-16 10:58:46 +0900

    Add files via upload

---
+++ JSPCoding/servletExample/src/main/java/com/jdbc/ChungnamServlet2.java
@@ -0,0 +1,36 @@
+package com.jdbc;
+
+import java.io.IOException;
+//import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.chungnam.ChungnamDAO;
+import com.chungnam.ChungnamVO;
+
+@WebServlet("/view")
+public class ChungnamServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		
+		ChungnamDAO dao = ChungnamDAO.getInstance();
+		ArrayList<ChungnamVO> list = dao.getChungnamList();
+		
+		request.setAttribute("list", list);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("jdbc/chungnamView.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
