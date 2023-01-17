aba6a38c77a513c7b6abb18dd8e49b34a1a065ad
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-01-17 15:20:43 +0900

    Update SubjectDAO.java

--- JSPCoding/scriptTag/src/main/java/com/subject/SubjectDAO.java
+++ JSPCoding/scriptTag/src/main/java/com/subject/SubjectDAO.java
@@ -3,40 +3,42 @@ package com.subject;
 import java.sql.*;
 import java.util.ArrayList;
 
+import static com.common.JDBCTemplate.*;
+
 /* DAO(Data Access Object) 클래스가 데이터 처리의 궁극적인 단계이다.
  * CRUD 프로그램 구현
  * 기본적인 데이터 처리 기능인 입력(create, insert), 조회(read(또는 retrieve), select), 수정(update),
  * 삭제(delete) 기능을 구현한 데이터베이스 프로그램*/
 public class SubjectDAO {
-    // 데이터베이스 연결 관련 상수 선언
-    private static final String JDBC_URL = "jdbc:oracle:thin:@127.0.0.1:1521/xepdb1";
-    private static final String USER = "javauser";
-    private static final String PASSWD = "java1234";
-
-    // 하나의 인스턴스만 가지게 하기 위해 싱글톤으로 생성
-    // 클래스 자신의 타입으로 정적 필드 선언
-    private static SubjectDAO instance = null;
-    // 외부에서 호출할 수 있는 정적 메소드인 getInstnace() 선언하여 인스턴스를 반환
-    public static SubjectDAO getInstance(){
-        if(instance == null)
-            instance = new SubjectDAO();
-        return instance;
-    }
-
-    // 외부에서 new 연산자로 생성자를 호출할 수 없도록 막기 위해 접근제어자 설정
-    private SubjectDAO(){
-        try {
-            Class.forName("oracle.jdbc.OracleDriver");
-        } catch(ClassNotFoundException e){
-            e.printStackTrace();
-        }
-    }
-
-    // Connection 객체 반환하는 메서드
-    private Connection getConnection() throws SQLException {
-        Connection con = DriverManager.getConnection(JDBC_URL, USER, PASSWD);
-        return con;
-    }
+//    // 데이터베이스 연결 관련 상수 선언
+//    private static final String JDBC_URL = "jdbc:oracle:thin:@127.0.0.1:1521/xepdb1";
+//    private static final String USER = "javauser";
+//    private static final String PASSWD = "java1234";
+//
+//    // 하나의 인스턴스만 가지게 하기 위해 싱글톤으로 생성
+//    // 클래스 자신의 타입으로 정적 필드 선언
+//    private static SubjectDAO instance = null;
+//    // 외부에서 호출할 수 있는 정적 메소드인 getInstnace() 선언하여 인스턴스를 반환
+//    public static SubjectDAO getInstance(){
+//        if(instance == null)
+//            instance = new SubjectDAO();
+//        return instance;
+//    }
+//
+//    // 외부에서 new 연산자로 생성자를 호출할 수 없도록 막기 위해 접근제어자 설정
+//    private SubjectDAO(){
+//        try {
+//            Class.forName("oracle.jdbc.OracleDriver");
+//        } catch(ClassNotFoundException e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    // Connection 객체 반환하는 메서드
+//    private Connection getConnection() throws SQLException {
+//        Connection con = DriverManager.getConnection(JDBC_URL, USER, PASSWD);
+//        return con;
+//    }
 
     // 학과 테이블에서 모든 레코드를 반환하는 메서드
     /* @return ArrayList<SubjectVO> 자료형 리턴
@@ -78,13 +80,17 @@ public class SubjectDAO {
         } catch(Exception e){
             System.out.println("ERROR=["+e+"]");
         } finally {
-            try {
-                if(rs != null) rs.close();
-                if(pstmt != null) pstmt.close();
-                if(conn != null) conn.close();
-            } catch (SQLException e){
-                System.out.println("DB 연동 해제 ERROR = ["+e+"]");
-            }
+//            try {
+//                if(rs != null) rs.close();
+//                if(pstmt != null) pstmt.close();
+//                if(conn != null) conn.close();
+//            } catch (SQLException e){
+//                System.out.println("DB 연동 해제 ERROR = ["+e+"]");
+//            }
+        	
+        	close(rs);
+        	close(pstmt);
+        	close(conn);
         }
         return list;
     }
@@ -115,13 +121,17 @@ public class SubjectDAO {
         } catch(Exception e){
             System.out.println("ERROR=["+e+"]");
         } finally {
-            try {
-                if(rs != null) rs.close();
-                if(pstmt != null) pstmt.close();
-                if(conn != null) conn.close();
-            } catch (SQLException e){
-                System.out.println("DB 연동 해제 ERROR = ["+e+"]");
-            }
+//            try {
+//                if(rs != null) rs.close();
+//                if(pstmt != null) pstmt.close();
+//                if(conn != null) conn.close();
+//            } catch (SQLException e){
+//                System.out.println("DB 연동 해제 ERROR = ["+e+"]");
+//            }
+        	
+        	close(rs);
+        	close(pstmt);
+        	close(conn);
         }
         return snum;
     }
@@ -145,21 +155,27 @@ public class SubjectDAO {
             pstmt.setString(1, svo.getS_num());
             pstmt.setString(2, svo.getS_name());
 
-            int insertCount = pstmt.executeUpdate();
+            int insertCount = pstmt.executeUpdate(); // 쿼리문 실행 - 입력이 정상적으로 완료되면 적용된 행의 수인 1 반환
 
-            if (insertCount == 1) // 입력이 정상적으로 완료되면 적용된 행의 수인 1 반환
+            if (insertCount == 1) {
                 success = true;
+                commit(conn);
+            }
         } catch(SQLException se){
                 System.out.println("입력에 문제가 있어 잠시 후에 다시 진행해주세요");
+                rollback(conn);
         } catch(Exception e){
                 System.out.println("ERROR=[" + e + "]");
+                rollback(conn);
         } finally{
-            try {
-                if (pstmt != null) pstmt.close();
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                System.out.println("DB 연동 해제 ERROR = [" + e + "]");
-            }
+//            try {
+//                if (pstmt != null) pstmt.close();
+//                if (conn != null) conn.close();
+//            } catch (SQLException e) {
+//                System.out.println("DB 연동 해제 ERROR = [" + e + "]");
+//            }
+        	close(pstmt);
+        	close(conn);
         }
         return success;
     }
