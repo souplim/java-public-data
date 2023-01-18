3e1c5e80c1e2b786243638370e4d5c1dac0c0e78
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-01-18 15:20:15 +0900

    Add files via upload

---
+++ JSPCoding/scriptTag/src/main/java/com/board/BoardWriteServlet.java
@@ -0,0 +1,44 @@
+package com.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/boardInsert")
+public class BoardWriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		Board board = new Board();
+		
+		String boardWriter = request.getParameter("boardWriter");
+		String boardTitle = request.getParameter("boardTitle");
+		String boardContent = request.getParameter("boardContent");
+		
+		board.setBoardWriter(boardWriter);
+		board.setBoardTitle(boardTitle);
+		board.setBoardContent(boardContent);
+		
+		BoardService boardService = BoardService.getInstance();
+		boolean result = boardService.insertBoard(board);
+		
+		if(result)
+			response.sendRedirect("board/boardList.jsp");
+		else {
+			request.setAttribute("message", "게시글 등록 실패!");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("views/common/error.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
