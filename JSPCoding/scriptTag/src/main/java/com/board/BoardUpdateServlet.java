e19812190e0a2dc08b3ec9825f6859405adb00fe
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-01-20 01:52:07 +0900

    Update BoardUpdateServlet.java

--- JSPCoding/scriptTag/src/main/java/com/board/BoardUpdateServlet.java
+++ JSPCoding/scriptTag/src/main/java/com/board/BoardUpdateServlet.java
@@ -21,10 +21,12 @@ public class BoardUpdateServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		Board board = new Board();
 		
+		int	boardNum = Integer.parseInt(request.getParameter("boardNum"));
 		String boardWriter = request.getParameter("boardWriter");
 		String boardTitle = request.getParameter("boardTitle");
 		String boardContent = request.getParameter("boardContent");
 		
+		board.setBoardNum(boardNum);
 		board.setBoardWriter(boardWriter);
 		board.setBoardTitle(boardTitle);
 		board.setBoardContent(boardContent);
@@ -33,10 +35,10 @@ public class BoardUpdateServlet extends HttpServlet {
 		boolean result = boardService.updateBoard(board);
 		
 		if(result)
-			response.sendRedirect("<%= request.getContextPath() %>/boardDetail");
+			response.sendRedirect("/scriptTag/board/boardList.jsp");
 		else {
 			request.setAttribute("message", "게시글 수정 실패!");
-			RequestDispatcher dispatcher = request.getRequestDispatcher("views/common/error.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/scriptTag/board/error.jsp");
 			dispatcher.forward(request, response);
 		}
 		
