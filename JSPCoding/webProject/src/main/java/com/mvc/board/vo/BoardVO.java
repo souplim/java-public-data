28702628aa6d1cf13dcfdf44390170e85eaee381
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-01-30 09:52:33 +0900

    Update BoardVO.java

--- JSPCoding/webProject/src/main/java/com/mvc/board/vo/BoardVO.java
+++ JSPCoding/webProject/src/main/java/com/mvc/board/vo/BoardVO.java
@@ -1,22 +1,39 @@
 package com.mvc.board.vo;
 
 public class BoardVO {
-	private int num;
-	private String author;
-	private String title;
-	private String content;
-	private String date;
-	private String passwd;
+	private int num;			// 글 번호
+	private String author;		// 글 작성자
+	private String title;		// 글제목
+	private String content;		// 글내용
+	private int readcnt;		// 글 조회수
+	private String writeday;	// 글 작성일
+	private int repRoot;		// 답변글 작성 시 사용(원래글의 번호 참조)
+	private int repStep;		// 답변글 작성 시 사용(답변글의 들여쓰기 지정)
+	private int repIndent;		// 답변글 작성 시 사용(답변글의 순서 지정)
+	private String passwd;		// 비밀번호
+	private int rCount;			// 댓글 개수
+	
+	// 조건검색시 사용할 속성
+	private String search = "";	// 기본값을 null이 아니라 ""(빈문자)로 제어. 검색 대상
+	private String keyword = "";// 검색어
 	
 	public BoardVO() {}
-	public BoardVO(int num, String author, String title, String content, String date, String passwd) {
+	public BoardVO(int num, String author, String title, String content, int readcnt, String writeday, int repRoot,
+			int repStep, int repIndent, String passwd, int rCount, String search, String keyword) {
 		super();
 		this.num = num;
 		this.author = author;
 		this.title = title;
 		this.content = content;
-		this.date = date;
+		this.readcnt = readcnt;
+		this.writeday = writeday;
+		this.repRoot = repRoot;
+		this.repStep = repStep;
+		this.repIndent = repIndent;
 		this.passwd = passwd;
+		this.rCount = rCount;
+		this.search = search;
+		this.keyword = keyword;
 	}
 	
 	public int getNum() {
@@ -43,11 +60,35 @@ public class BoardVO {
 	public void setContent(String content) {
 		this.content = content;
 	}
-	public String getDate() {
-		return date;
+	public int getReadcnt() {
+		return readcnt;
 	}
-	public void setDate(String date) {
-		this.date = date;
+	public void setReadcnt(int readcnt) {
+		this.readcnt = readcnt;
+	}
+	public String getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(String writeday) {
+		this.writeday = writeday;
+	}
+	public int getRepRoot() {
+		return repRoot;
+	}
+	public void setRepRoot(int repRoot) {
+		this.repRoot = repRoot;
+	}
+	public int getRepStep() {
+		return repStep;
+	}
+	public void setRepStep(int repStep) {
+		this.repStep = repStep;
+	}
+	public int getRepIndent() {
+		return repIndent;
+	}
+	public void setRepIndent(int repIndent) {
+		this.repIndent = repIndent;
 	}
 	public String getPasswd() {
 		return passwd;
@@ -55,10 +96,30 @@ public class BoardVO {
 	public void setPasswd(String passwd) {
 		this.passwd = passwd;
 	}
+	public int getrCount() {
+		return rCount;
+	}
+	public void setrCount(int rCount) {
+		this.rCount = rCount;
+	}
+	public String getSearch() {
+		return search;
+	}
+	public void setSearch(String search) {
+		this.search = search;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
 	
 	@Override
 	public String toString() {
-		return "BoardVO [num=" + num + ", author=" + author + ", title=" + title + ", content=" + content + ", date="
-				+ date + ", passwd=" + passwd + "]";
+		return "BoardVO [num=" + num + ", author=" + author + ", title=" + title + ", content=" + content + ", readcnt="
+				+ readcnt + ", writeday=" + writeday + ", repRoot=" + repRoot + ", repStep=" + repStep + ", repIndent="
+				+ repIndent + ", passwd=" + passwd + ", rCount=" + rCount + ", search=" + search + ", keyword="
+				+ keyword + "]";
 	}
 }
