22115369f13f984f2e7a2d765ec592d69c348ae3
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-02-06 18:09:14 +0900

    Delete JSPCoding/webProject/src/main directory

--- JSPCoding/webProject/src/main/java/com/mvc/common/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.mvc.common.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Controller {
-	String execute(HttpServletRequest request, HttpServletResponse response);
-}
