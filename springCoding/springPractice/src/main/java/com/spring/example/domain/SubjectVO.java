4297cdb4d3c4db6233c80c8cc64e1e358f2d6cb8
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-02-15 01:37:51 +0900

    Add files via upload

---
+++ springCoding/springPractice/src/main/java/com/spring/example/domain/SubjectVO.java
@@ -0,0 +1,14 @@
+package com.spring.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // 설정자, 접근자, toString(), hashCode(), 기본생성자
+@AllArgsConstructor // 매개변수 있는 생성자
+@NoArgsConstructor  // 매개변수 있는 생성자 만들면 @Data에서 기본생성자 안 만들어지므로 추가
+public class SubjectVO {
+	private int no;
+	private String s_num;
+	private String s_name;
+}
