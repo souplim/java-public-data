8f35b5897c415417e407d6c858e2d097d1b1a9ce
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-02-16 09:52:59 +0900

    Update common.js

--- springCoding/springPractice/src/main/webapp/resources/js/common.js
+++ springCoding/springPractice/src/main/webapp/resources/js/common.js
@@ -2,7 +2,6 @@
  * 출력영역 : alert
  * 예시 : if(!chkData("#keyword","검색이름")) return;
  */
- 
 function chkData(item, msg){
 	if($(item).val().replace(/s/g,"")==""){
 		alert(msg+"입력해 주세요.");
@@ -21,6 +20,20 @@ function dataCheck(item, area, msg){
 	if($(item).val().replace(/s/g,"")==""){
 		$(area).text(msg+"입력해 주세요.").css("color","red");
 		$(item).select();
+		return false;
+	} else
+		return true;
+}
+
+/* 함수명 : checkForm(유효성 체크 대상, 메시지 내용)
+ * 출력영역 : placeholder 속성 이용
+ * 예시 : if(!checkForm("#keyword","검색이름")) return;
+ */
+function checkForm(item, msg){
+	let message = "";
+	if($(item).val().replace(/\s/g,"")==""){
+		message = msg + " 입력해주세요.";
+		$(item).attr("placeholder",message);
 		return false;
 	} else
 		return true;
