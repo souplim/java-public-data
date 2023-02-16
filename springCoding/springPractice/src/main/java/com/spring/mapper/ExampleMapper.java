4d132c751b1168215f476a172fb9757c232ee99e
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-02-16 09:48:13 +0900

    Update ExampleMapper.java

--- springCoding/springPractice/src/main/java/com/spring/mapper/ExampleMapper.java
+++ springCoding/springPractice/src/main/java/com/spring/mapper/ExampleMapper.java
@@ -7,4 +7,12 @@ import com.spring.example.domain.SubjectVO;
 public interface ExampleMapper {
 
 	public List<SubjectVO> getSubjectList(int no);
+	
+	public int subjectInsert(SubjectVO subject);
+	
+	public int subjectUpdate(SubjectVO subject);
+	
+	public int subjectDelete(int no);
+	
+	public String getSubjectName(String s_num); /* 학과번호를 인자로 학과명을 출력 */
 }
