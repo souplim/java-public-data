0e2f8153ff9799ef80389df57f095490c6cbfff0
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-03-15 14:47:58 +0900

    Update DataServiceImpl.java
    detail 메서드 추가

--- springBootCoding/springProject/src/main/java/com/spring/openapi/data/service/DataServiceImpl.java
+++ springBootCoding/springProject/src/main/java/com/spring/openapi/data/service/DataServiceImpl.java
@@ -8,12 +8,15 @@ import java.net.URLEncoder;
 
 import org.springframework.stereotype.Service;
 
+import com.spring.common.openapi.URLConnectUtil;
+import com.spring.openapi.data.vo.OpenApiDTO;
+
 @Service
 public class DataServiceImpl implements DataService {
 	
 	@Override
 	public StringBuffer chungnamList() throws Exception {
-		//String site = "https://tour.chungnam.go.kr/_prog/openapi/?func=tour&start=1&end=10";
+		//오픈 api : "https://tour.chungnam.go.kr/_prog/openapi/?func=tour&start=1&end=10";
 		
 		// 전달해주어야 하는 파라미터
 		StringBuffer site = new StringBuffer("https://tour.chungnam.go.kr/_prog/openapi/");
@@ -25,6 +28,7 @@ public class DataServiceImpl implements DataService {
 		URL url = new URL(site.toString());
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		
+		/*
 		// 요청방식 선택(GET, POST)
 		conn.setRequestMethod("GET");
 		// 서버 Response Data를 xml(application/xml), json(application/json) 형식의 타입으로 요청
@@ -42,5 +46,28 @@ public class DataServiceImpl implements DataService {
 			output.append(inputLine); // 출력스트림에 추가
 		}
 		return output;
+		*/
+		
+		// 위 코드 클래스의 메서드로 분리하기
+		OpenApiDTO openApi = new OpenApiDTO(site.toString(), "GET", "application/xml", null);
+		StringBuffer result = URLConnectUtil.openAPIData(openApi);
+		return result;
+	}
+
+	@Override
+	public StringBuffer chungnamDetail(int mng_no) throws Exception {
+		// 전달해주어야 하는 파라미터
+		StringBuffer site = new StringBuffer("https://tour.chungnam.go.kr/_prog/openapi/");
+		site.append("?"+URLEncoder.encode("func","UTF-8")+"="+URLEncoder.encode("tour","UTF-8"));
+		site.append("&"+URLEncoder.encode("mode","UTF-8")+"="+URLEncoder.encode("V","UTF-8"));
+		site.append("&"+URLEncoder.encode("mng_no","UTF-8")+"="+URLEncoder.encode(mng_no+"","UTF-8"));
+		
+		// URL 클래스 -> 참조된 자원에 대해 입출력 스트림을 형성할 수 있음
+		URL url = new URL(site.toString());
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		
+		OpenApiDTO openApi = new OpenApiDTO(site.toString(), "GET", "application/xml", null);
+		StringBuffer result = URLConnectUtil.openAPIData(openApi);
+		return result;
 	}
 }
