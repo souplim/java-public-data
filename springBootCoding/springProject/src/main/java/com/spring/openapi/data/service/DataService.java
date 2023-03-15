9249a124bdee79fbf4d8e118b6f16ace0534afc5
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-03-15 14:47:34 +0900

    Update DataService.java
    detail 메서드 추가

--- springBootCoding/springProject/src/main/java/com/spring/openapi/data/service/DataService.java
+++ springBootCoding/springProject/src/main/java/com/spring/openapi/data/service/DataService.java
@@ -2,4 +2,6 @@ package com.spring.openapi.data.service;
 
 public interface DataService {
 	public StringBuffer chungnamList() throws Exception; // 네트워크 -> 입출력 -> 예외처리 필요. 상위로 위임
+
+	public StringBuffer chungnamDetail(int mgn_no) throws Exception;
 }
