17970b70aea4a6e8962609bc0229193aee82814d
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-03-16 13:41:04 +0900

    Update DataService.java

--- springBootCoding/springProject/src/main/java/com/spring/openapi/data/service/DataService.java
+++ springBootCoding/springProject/src/main/java/com/spring/openapi/data/service/DataService.java
@@ -3,5 +3,7 @@ package com.spring.openapi.data.service;
 public interface DataService {
 	public StringBuffer chungnamList() throws Exception; // 네트워크 -> 입출력 -> 예외처리 필요. 상위로 위임
 
-	public StringBuffer chungnamDetail(int mgn_no) throws Exception;
+	public StringBuffer chungnamDetail(String mgn_no) throws Exception;
+
+	public StringBuffer animalDaejeonList() throws Exception;
 }
