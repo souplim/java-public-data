bd792b0f50f9ff7a27f1120ea385df752d3b267d
yoonjae <107927616+eunja2@users.noreply.github.com>
2023-03-16 13:40:27 +0900

    Update DataController.java

--- springBootCoding/springProject/src/main/java/com/spring/openapi/data/controller/DataController.java
+++ springBootCoding/springProject/src/main/java/com/spring/openapi/data/controller/DataController.java
@@ -2,6 +2,7 @@ package com.spring.openapi.data.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +20,7 @@ public class DataController {
 	@Setter(onMethod_ = @Autowired)
 	private DataService dataService;
 	
-	@RequestMapping(value="/chungnamView")
+	@GetMapping(value="/chungnamView")
 	public String chungnamView() {
 		log.info("충남 관광 명소 리스트 화면");
 		
@@ -27,14 +28,14 @@ public class DataController {
 	}
 	
 	@ResponseBody
-	@RequestMapping(value="/chungnamList", produces="application/xml; charset=UTF-8")
+	@GetMapping(value="/chungnamList", produces="application/xml; charset=UTF-8")
 	public String chungnamList() throws Exception {
 		log.info("충남 관광 명소 리스트");
 		StringBuffer sb = dataService.chungnamList();
 		return sb.toString();
 	}
 	
-	@RequestMapping(value="/chungnamDetailView")
+	@GetMapping(value="/chungnamDetailView")
 	public String chungnamDetailView() {
 		log.info("충남 관광 명소 디테일 화면");
 		
@@ -42,10 +43,24 @@ public class DataController {
 	}
 	
 	@ResponseBody
-	@RequestMapping(value="/chungnamDetail", produces="application/xml; charset=UTF-8")
-	public String chungnamDetail(@RequestParam("mng_no") int mng_no) throws Exception {
+	@GetMapping(value="/chungnamDetail", produces="application/xml; charset=UTF-8")
+	public String chungnamDetail(@RequestParam("mng_no") String mng_no) throws Exception {
 		log.info("충남 관광 명소 디테일");
 		StringBuffer sb = dataService.chungnamDetail(mng_no);
+		return sb.toString();
+	}
+	
+	@GetMapping(value="/animalDaejeonView")
+	public String animalDaejeonView() {
+		log.info("대전 유기동물공고 리스트 화면");
+		return "data/animalDaejeonView"; // WEB-INF/views/data/animalDaejeonView.jsp
+	}
+	
+	@ResponseBody
+	@GetMapping(value="/animalDaejeonList", produces = "application/xml; charset=UTF-8")
+	public String animalDaejeonList() throws Exception {
+		log.info("대전 유기동물 공고 리스트");
+		StringBuffer sb = dataService.animalDaejeonList();
 		return sb.toString();
 	}
 }
